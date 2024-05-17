d9969bee6259284719099c6b6fce59c03c7e7eca
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-18 02:48:32 +0300

    CH9

--- Easy/BitwiseOperators/BitwiseOperators.c
+++ Easy/BitwiseOperators/BitwiseOperators.c
@@ -6,7 +6,28 @@
 //Complete the following function.
 void calculate_the_maximum(int n, int k) {
   //Write your code here.
-
+  int maxAnd = 0, maxOr = 0, maxXor = 0;
+  int And       , Or       , Xor       ;
+  
+  for (int i = 1; i < n; i++) {
+    for (int j = (i + 1); j <= n; j++) {
+        And = i & j;    Or  = i | j;    Xor = i ^ j;
+        
+        if((And < k) && (And > maxAnd)) {
+            maxAnd = And;
+        }
+        
+        if((Or  < k) && (Or  > maxOr )) {
+            maxOr  = Or ;
+        }
+        
+        if((Xor < k) && (Xor > maxXor)) {
+            maxXor = Xor;
+        }
+    }
+  }
+  
+  printf("%d\n%d\n%d\n", maxAnd, maxOr, maxXor);
 }
 
 int main() {
