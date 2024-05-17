69b5f2321a3652260cbd2084ec08a430cd470b10
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-17 21:59:51 +0300

    CH8

--- Easy/SumofDigitsofaFiveDigitNumber/SumofDigitsofaFiveDigitNumber.c
+++ Easy/SumofDigitsofaFiveDigitNumber/SumofDigitsofaFiveDigitNumber.c
@@ -7,9 +7,17 @@ int main() {
 	
     int n;
     scanf("%d", &n);
-
-    //Complete the code to calculate the sum of the five digits on n.
     
-
+    //Complete the code to calculate the sum of the five digits on n.
+    int firstDigit  = ((n          )    / 10000);
+    int secondDigit = ((n  % 10000)     / 1000 );
+    int thirdDigit  = ((n  % 1000)      / 100  );
+    int fourthDigit = ((n  % 100)       / 10   );
+    int fifthDigit  = ((n  % 10)        / 1    );
+    
+    int Sumof5Digits = firstDigit + secondDigit + thirdDigit + fourthDigit + fifthDigit;
+    
+    printf("%d", Sumof5Digits);
+    
     return 0;
 }
