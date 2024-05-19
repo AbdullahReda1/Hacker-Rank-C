270a383dba34e72e7aebc9788a3938ecd2420ef2
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-19 10:30:05 +0300

    CH12

--- Easy/BoxesThroughATunnel/BoxesThroughATunnel.c
+++ Easy/BoxesThroughATunnel/BoxesThroughATunnel.c
@@ -8,7 +8,9 @@ struct box
 	/**
 	* Define three fields of type int: length, width and height
 	*/
-    
+    int length;
+    int width ;
+    int height;
 };
 
 typedef struct box box;
@@ -17,14 +19,14 @@ int get_volume(box b) {
 	/**
 	* Return the volume of the box
 	*/
-
+    return (b.length * b.width * b.height);
 }
 
 int is_lower_than_max_height(box b) {
 	/**
 	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
 	*/
-
+    return ((b.height < MAX_HEIGHT)? 1: 0);
 }
 
 int main()
