b6af243eb695eba4d4fc23dce723a54a55fcfdbe
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-19 09:09:42 +0300

    CH10

--- Easy/CalculatetheNthterm/CalculatetheNthterm.c
+++ Easy/CalculatetheNthterm/CalculatetheNthterm.c
@@ -6,6 +6,10 @@
 //Complete the following function.
 int find_nth_term(int n, int a, int b, int c) {
 	//Write your code here.
+	/* 3 is the key of the series, where the otherwise condition is upper than 3 */
+	/* so, if this number changed according to the series, this will change 1st condition (n > 3)
+	       and number of operations (3 additions - also the operation is according to the series type -)
+		   and Nth back into series (n - 3 - into the recursion function -) and number of sequence elements ([3]) */
 	while (n >= 1 && n <= 20) {
 		if (n > 3) {
 			a += b + c;
