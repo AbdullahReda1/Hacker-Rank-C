060d6a6c61a1756f14e07a21f44659aaf7d03932
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-19 09:54:14 +0300

    CH11

--- Easy/StudentsMarksSum/StudentsMarksSum.c
+++ Easy/StudentsMarksSum/StudentsMarksSum.c
@@ -6,7 +6,17 @@
 //Complete the following function.
 int marks_summation(int* marks, int number_of_students, char gender) {
     //Write your code here.
-	
+    int sum = 0;
+    if (gender == 'b') {
+        for (int i = 0; i < number_of_students; i+=2) {
+            sum += marks[i];
+        }
+    } else if ((gender == 'g') && (number_of_students > 1)) {
+        for (int i = 1; i < number_of_students; i+=2) {
+            sum += marks[i];
+        }
+    }
+    return  sum;
 }
 
 int main() {
