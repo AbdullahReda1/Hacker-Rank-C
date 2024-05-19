91657f09acb6f3f677418d932c24fc8fd76294dc
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-19 08:41:35 +0300

    CH10

--- Easy/CalculatetheNthterm/CalculatetheNthterm.c
+++ Easy/CalculatetheNthterm/CalculatetheNthterm.c
@@ -5,8 +5,19 @@
 
 //Complete the following function.
 int find_nth_term(int n, int a, int b, int c) {
-  //Write your code here.
-
+	//Write your code here.
+	while (n >= 1 && n <= 20) {
+		if (n > 3) {
+			a += b + c;
+			b += a + c;
+			c += a + b;
+			return find_nth_term(n - 3, a, b, c);
+		} else {
+			int sequence[3] = {a, b, c};
+			return sequence[n - 1];
+		}
+	}
+	return 0;
 }
 
 int main() {
