b0a46796b6abae7bc39f96c49702d7aa7e42d374
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-26 17:29:17 +0300

    CH1

--- Medium/PrintingPatternUsingLoops/PrintingPatternUsingLoops.c
+++ Medium/PrintingPatternUsingLoops/PrintingPatternUsingLoops.c
@@ -10,7 +10,19 @@ int main()
     scanf("%d", &n);
 
   	// Complete the code to print the pattern.
-    
+    for (int i = n; i >= -n; i--) {
+        if (i == 0) {
+            i -= 2;
+        }
+        for (int j = n; j >= -n; j--) {
+            if (j == 0) {
+                j -= 2;
+            }
+            if   (abs(j) > abs(i)) { printf("%d ", abs(j)); }
+            else                   { printf("%d ", abs(i)); }
+        }
+        printf("\n");
+    }
 
     return 0;
 }
