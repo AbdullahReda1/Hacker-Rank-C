15529646c85da3f670c474fba72f2506e144bd8c
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-30 01:07:02 +0300

    CH5

--- Medium/DigitFrequency/DigitFrequency.c
+++ Medium/DigitFrequency/DigitFrequency.c
@@ -6,7 +6,22 @@
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char * s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
     
+    int arrCounter[10] = {0};
+    
+    int i = 0;
+    while (s[i] != NULL) {
+        if ((s[i] >= '0') && (s[i] <= '9')) {
+            arrCounter[(s[i] - 48)]++;
+        }
+        i++;
+    }
+    
+    for (int j = 0; j < 10; j++) {
+        printf("%d ", arrCounter[j]);
+    }
     
     return 0;
 }
