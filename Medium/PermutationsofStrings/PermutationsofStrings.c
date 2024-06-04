9276765b86e5d39f3136085d7f9ee0a9545b132e
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-06-05 02:50:10 +0300

    CH7

--- Medium/PermutationsofStrings/PermutationsofStrings.c
+++ Medium/PermutationsofStrings/PermutationsofStrings.c
@@ -9,7 +9,37 @@ int next_permutation(int n, char **s)
 	* Return 0 when there is no next permutation and 1 otherwise
 	* Modify array s to its next permutation
 	*/
-    
+
+    // Traverse the array from right to left to find the first pair of elements
+    // where the previous element is smaller than the current element.
+    for (int i = n - 2; i >= 0; i--) {
+        // Compare the current element with the next element.
+        if (strcmp(s[i], s[i + 1]) < 0) {
+            // If such a pair is found, traverse the array from right to left again
+            // to find the first element that is greater than the current element.
+            for (int j = n - 1; j > i; j--) {
+                if (strcmp(s[j], s[i]) > 0) {
+                    // Swap these two elements.
+                    char *temp = s[i];
+                    s[i] 	   = s[j];
+                    s[j] 	   = temp;
+
+                    // Reverse the sequence of elements to the right of the current element.
+                    for (int left = i + 1, right = n - 1; left < right; left++, right--) {
+                        char *temp = s[left] ;
+                        s[left]    = s[right];
+                        s[right]   = temp	 ;
+                    }
+                    
+                    // Return 1 to indicate that the next permutation was found.
+                    return 1;
+                }
+            }
+        }
+    }
+
+    // Return 0 to indicate that no next permutation was found (already at the last permutation).
+    return 0;
 }
 
 int main()
