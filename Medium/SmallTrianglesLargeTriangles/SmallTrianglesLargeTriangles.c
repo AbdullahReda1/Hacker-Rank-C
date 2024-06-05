7b678ce526eabfd91f5fc2a4700360c811b92832
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-06-05 22:02:53 +0300

    CH9

--- Medium/SmallTrianglesLargeTriangles/SmallTrianglesLargeTriangles.c
+++ Medium/SmallTrianglesLargeTriangles/SmallTrianglesLargeTriangles.c
@@ -15,7 +15,31 @@ void sort_by_area(triangle* tr, int n) {
 	/**
 	* Sort an array a of the length n
 	*/
-    
+    // Allocate an array to store the areas of the triangles
+    double *area = (double *)malloc(n * sizeof(double));
+
+    for (int i = 0; i < (n); i++) {
+    	double p = 0.0;
+    	p = ((tr[i].a) + (tr[i].b) + (tr[i].c)) / 2.0;
+    	area[i] =
+    	sqrt(p * (p - (tr[i].a)) * (p - (tr[i].b)) * (p - (tr[i].c)));
+    }
+
+    // Bubble sort to sort triangles by area
+    for (int i = 0; i < (n - 1); i++) {
+        for (int j = 0; j < (n - i - 1); j++) {
+        	if (area[j] > area[j + 1]) {
+        		double temp_area = area[j];
+        		area[j] = area[j + 1];
+        		area[j + 1] = temp_area;
+
+        		// Swap the corresponding triangles
+        		triangle temp = tr[j];
+        		tr[j] = tr[j + 1];
+        		tr[j + 1] = temp;
+        	}
+        }
+    }
 }
 
 int main()
