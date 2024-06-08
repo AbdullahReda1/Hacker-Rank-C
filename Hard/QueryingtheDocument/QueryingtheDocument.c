cb1d3dca1badacf4a4bf2463fc28595fa1fb1241
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-06-08 23:43:12 +0300

    CH2

--- Hard/QueryingtheDocument/QueryingtheDocument.c
+++ Hard/QueryingtheDocument/QueryingtheDocument.c
@@ -6,15 +6,15 @@
 #define MAX_PARAGRAPHS 5
 
 char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
-    
+    return document[n - 1][m - 1][k - 1];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
-    
+    return document[m - 1][k - 1];
 }
 
 char*** kth_paragraph(char**** document, int k) {
-    
+    return document[k - 1];
 }
 
 char**** get_document(char* text) {
